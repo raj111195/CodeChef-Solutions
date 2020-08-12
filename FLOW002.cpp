dc8c059c55ad599741ecfce3b745fc66c3f74eaf
Rajkumar S <rajkumar.df54@gmail.com>
2020-08-13 00:26:15 +0530

    Add files via upload

---
+++ FLOW002.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+ int t,i,x,n,z;
+ 
+
+cin>>t;
+
+ while(t--)
+ {
+     
+     cin>>n>>x;
+     
+     
+  cout<<n%x ;
+  cout<<"\n";
+}	return 0;
+}
+
