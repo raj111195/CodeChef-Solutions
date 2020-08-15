6c5929f40d4217b78998f89d73234ec71523d749
Rajkumar S <rajkumar.df54@gmail.com>
2020-08-15 23:04:20 +0530

    Add files via upload

---
+++ FSORT.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include<math.h>
+
+using namespace std;
+
+int main() {
+
+ int t,z;
+ 
+
+cin>>t;
+
+ while(t--)
+ {
+     
+         cin>>z;
+         
+         cout<<int(sqrt(z));
+        
+     
+  cout<<"\n";
+}	return 0;
+}
+
