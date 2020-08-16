8933ac54cc7beb0f8281d3cb8a715493d973b288
Rajkumar S <rajkumar.df54@gmail.com>
2020-08-16 23:38:57 +0530

    Add files via upload

---
+++ CHDOGS.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include<vector>
+#include<stdio.h>
+using namespace std;
+
+int main() {
+
+ double a,b,c=(double)2/(double)3;
+ int t,i,x,n,z;
+ 
+vector<int> v;
+cin>>t;
+
+ while(t--)
+ {
+     
+     cin>>a>>b;
+     
+     
+     //cout<<c*a/b;
+     printf("%.15lf\n",c*a/b);
+  
+}	return 0;
+}
+
