d403a980b9d99e0e36e260c997a5997934ee1f0f
Rajkumar S <rajkumar.df54@gmail.com>
2020-08-26 02:56:21 +0530

    Add files via upload

---
+++ CHEFSOCK.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+ long long t,i,x;
+ 
+
+cin>>t>>i>>x;
+
+  if(((x-t)/i)%2)
+  cout<<"Unlucky Chef";
+  else cout<<"Lucky Chef";
+ 
+ 	return 0;
+}
+
