bb9b13357503a8e7037a483f80f701b11f6e6485
Rajkumar S <rajkumar.df54@gmail.com>
2020-09-14 22:34:18 +0530

    Add files via upload

---
+++ CHCOINSG.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+int main() {
+
+ long long int t,n;
+ 
+//vector<int> v;
+cin>>t;
+
+ while(t--)
+ {
+     
+     cin>>n;
+     
+     if(n%6)
+     cout<<"Chef";
+     else cout<<"Misha";
+     
+  cout<<"\n";
+}	return 0;
+}
+
